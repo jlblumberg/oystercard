f21024e03240ca07bf9ad304ed18c2303cbe4a37
Ziad <ziad1991@hotmail.co.uk>
2020-01-27 23:31:05 +0000

    add functionality for in_journey state, touch  in, touch out, minimum balance & card limit

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,11 +1,35 @@
 class OysterCard
     attr_reader :balance
+    attr_writer :state
+
+    CARD_LIMIT = 90
+    MINIMUM_BALANCE = 1
 
     def initialize
         @balance = 0
+        @state = false
     end
 
     def top_up(amount)
+        raise "Cannot top up over the £#{CARD_LIMIT} limit!" if (@balance + amount) > CARD_LIMIT
         @balance += amount
     end
+
+    def deduct(fare)
+        @balance -= fare
+    end
+
+    def in_journey?
+        @state
+    end
+
+    def touch_in
+        raise "Insufficient funds for travel - £1 minimum required" if @balance < MINIMUM_BALANCE
+        @state = true
+    end
+
+    def touch_out
+        @state = false
+    end
+
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,8 +6,52 @@ describe OysterCard do
         expect(oyster.balance).to eq 0
     end
 
+    describe "#top_up" do
     it 'adds money to balance' do
-        expect { oyster.top_up(10) }.to change { oyster.balance }.from(0).to(10)
+        expect { oyster.top_up(10) }.to change { oyster.balance }.by(10)
+    end
+    
+        it 'raises an error if topping up over limit' do
+            oyster.top_up(OysterCard::CARD_LIMIT)
+            expect { oyster.top_up(1) }.to raise_error("Cannot top up over the £#{OysterCard::CARD_LIMIT} limit!")
+        end
+    end
+
+    it 'deducts the fare from the balance' do
+        oyster.top_up(OysterCard::CARD_LIMIT)
+        expect { oyster.deduct(10) }.to change { oyster.balance }.by(-10)
+    end
+
+    describe "#in_journey?" do
+        it 'returns false if not tapped in' do
+            expect(oyster.in_journey?).to eq false
+        end
+
+        it 'returns true if tapped in' do
+            oyster.state = true
+            expect(oyster.in_journey?).to eq true
+        end
+    end
+
+    describe "#touch_in" do
+        it 'should change state to true' do
+            oyster.top_up(OysterCard::MINIMUM_BALANCE)
+            oyster.touch_in
+            expect(oyster).to be_in_journey
+        end
+
+        it 'should raise an error if card does not hold the minimum balance' do
+            expect { oyster.touch_in }.to raise_error("Insufficient funds for travel - £1 minimum required")
+        end
+    end
+
+    describe "#touch_out" do
+        it 'should change state to false' do
+            oyster.top_up(OysterCard::MINIMUM_BALANCE)
+            oyster.touch_in
+            oyster.touch_out
+            expect(oyster).not_to be_in_journey
+        end
     end
 end
 
