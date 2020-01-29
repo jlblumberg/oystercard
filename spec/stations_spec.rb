7fe50f13646d40b679ac7c2441a64a5d9763ed99
Ziad <ziad1991@hotmail.co.uk>
2020-01-29 16:24:59 +0000

    write tests for zone & name instance variables

--- spec/stations_spec.rb
+++ spec/stations_spec.rb
@@ -1,3 +1,11 @@
 require 'stations'
 describe Stations do
+    subject(:station) { described_class.new }
+    it 'has an instance variable for name' do
+        expect(station.name).to eq "name"
+    end
+
+    it 'has an instance variable for zone' do
+        expect(station.zone).to eq "zone"
+    end
 end
