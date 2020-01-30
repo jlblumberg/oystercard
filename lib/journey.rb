5000181ed8e296167d38e5e8693c50372b019eae
Josh Blumberg <jlblumberg@gmail.com>
2020-01-30 16:15:45 +0000

    start again

---
+++ lib/journey.rb
@@ -0,0 +1,11 @@
+require_relative 'oyster_card'
+
+class Journey
+
+# starting a journey, 
+# finishing a journey, (touch_out)
+# calculating the fare of a journey, (#fare)
+# returning whether or not the journey is complete. (#entry, exit and error)
+
+
+end
