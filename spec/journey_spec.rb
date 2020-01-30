13dc7add85dda04a2f6c5cf7d0df0d7887837de4
Josh Blumberg <jlblumberg@gmail.com>
2020-01-30 17:03:56 +0000

    add penalty fare and test that journey has this by default

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -10,4 +10,14 @@ describe Journey do
   it "knows if a journey is not complete" do
     expect(journey.complete?).to eq(false)
   end
+
+  describe "#fare" do
+    let(:station) { double :station, zone: 1}
+    let(:other_station) { double :other_station }
+
+    it 'has a penalty fare by default' do
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
+    
+  end 
 end
