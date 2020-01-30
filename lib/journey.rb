19e70d13fb5baa56296d775e020fdf7282ffa823
Josh Blumberg <jlblumberg@gmail.com>
2020-01-30 17:02:15 +0000

    journey class and tests started

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,10 +2,26 @@ require_relative 'oyster_card'
 
 class Journey
 
+  attr_reader :entry_station
+
+  PENALTY_FARE = 6
+
+  def initialize(entry_station = nil)
+    @entry_station = entry_station
+    @complete = false
+  end
+
+  def fare
+    PENALTY_FARE
+  end
+
+  def complete?
+    @complete
+  end
+
 # starting a journey, 
 # finishing a journey, (touch_out)
 # calculating the fare of a journey, (#fare)
 # returning whether or not the journey is complete. (#entry, exit and error)
-
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,4 +1,13 @@
 require 'journey'
 
 describe Journey do
+  subject(:journey) { described_class.new(entry_station = "station")}
+
+  it "is created with an entry station" do
+    expect(journey.entry_station).to eq("station")
+  end
+
+  it "knows if a journey is not complete" do
+    expect(journey.complete?).to eq(false)
+  end
 end
